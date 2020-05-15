7540b308c183450aa1001e62b5767172b508bf03
Azog O Profano <andrade.wellington@gmail.com>
2020-05-15 20:20:01 -0300

    Inserida primeiro servlet

---
+++ src/br/com/gama/restful/servlet/teste/HelloServlet.java
@@ -0,0 +1,25 @@
+package br.com.gama.restful.servlet.teste;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("Olá mundo servlet");
+	}
+	
+
+}
