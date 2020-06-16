cf2cce52a0a237b3cae0020705c8f7b4643f3e40
Lord Kaecilius <agamottoseye@outlook.com>
2020-06-16 20:59:44 -0300

    Made till page 96. Azog, you must begin from 5.6 chapter, good luck

--- src/br/com/gama/restful/servlet/CarrosServlet.java
+++ src/br/com/gama/restful/servlet/CarrosServlet.java
@@ -9,11 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import br.com.gama.restful.modelo.Carro;
 import br.com.gama.restful.modelo.ListaCarros;
 import br.com.gama.restful.service.CarroService;
 import br.com.gama.restful.service.JAXBUtil;
 import br.com.gama.restful.service.ServletUtil;
+import br.com.gama.restful.util.RegexUtil;
 
 @WebServlet("/carros/*")
 public class CarrosServlet extends HttpServlet{
@@ -21,28 +25,50 @@ public class CarrosServlet extends HttpServlet{
 	private CarroService carroService=new CarroService();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String requestUri = req.getRequestURI();
+		Long id=RegexUtil.matchId(requestUri);
+		if(id !=null) {
+			//informou id
+			Carro carro=carroService.getCarroById(id);
+			if(carro !=null) {
+				Gson gson=new GsonBuilder().setPrettyPrinting().create();
+				String json=gson.toJson(carro);
+				ServletUtil.writeJSON(resp, json);
+			}else {
+				resp.sendError(404, "Carro não encontrado");
+			}
+		}else{
+				//lista de carros
+				List<Carro>carros=carroService.getCarros();
+				Gson gson=new GsonBuilder().setPrettyPrinting().create();
+				String json=gson.toJson(carros);
+				ServletUtil.writeJSON(resp, json);
+			}
+		}
+		
 		/*Populando a lista com registros do bd*/
-		List<Carro>carros=carroService.getCarros();
+		//List<Carro>carros=carroService.getCarros();
 		
 		/*Instanciando objeto que vai gerar meu XML*/
-		ListaCarros lista=new ListaCarros();
+		//ListaCarros lista=new ListaCarros();
 		
 		/*Adicionando minha lista no objeto*/
-		lista.setCarros(carros);
+		//lista.setCarros(carros);
 		
 		/*Gerando XML*/
-		String xml=JAXBUtil.toXML(lista);
+		//String xml=JAXBUtil.toXML(lista);
 		
 		/*Gerando JSON*/
-		String json=JAXBUtil.toJSON(lista);
+		//String json=JAXBUtil.toJSON(lista);
 		
 		/*Escrevendo o XML na response do servlet com application/xml*/
 		//ServletUtil.writeXML(resp, xml);
-		ServletUtil.writeJSON(resp, json);//(resp, xml);
+		//ServletUtil.writeJSON(resp, json);//(resp, xml);
 		
 	}
 	
 	
 	
 
-}
+
